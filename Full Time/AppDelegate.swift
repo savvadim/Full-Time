551424020e0145688b6f105d15ef64d3990f5273
Vadim Savosko <asphaltype@gmail.com>
2023-11-21 14:23:18 +0300

    launch

--- Full Time/AppDelegate.swift
+++ Full Time/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let viewController = ViewController()
+
         let mainViewController = LoginViewController(viewModel: .init())
 //        let mainViewController = TabBarController()
         
